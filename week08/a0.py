e4c086a189fbf47801e8bc3f59a333eec50b6d1c
GMack7 <gsm7xz@virginia.edu>
2025-02-05 14:08:20 -0500

    update

---
+++ week08/a0.py
@@ -0,0 +1,11 @@
+DENOMINATIONS = [100,20,10,5,1,0.25,0.1,0.5,0.01]
+
+def change(cost, payment):
+    # your code here
+    return list_of_change
+
+# Test cases
+print(change(5,2.55))
+print(change(2.55,5))
+print(change(5,5))
+print(change(0,5))
---
+++ week08/a1.py
@@ -0,0 +1,7 @@
+def find_sums(target, numbers):
+    pairs = []
+    # code goes here
+    return pairs
+
+print(find_sums(9,[0,1,3,6,7,8]))
+print(find_sums(11,[0,8,3,6,7,4]))
